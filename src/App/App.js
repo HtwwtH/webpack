c286cab2940eb3c3e9eee9c001b2e47379b3b537
HtwwtH <alina.l22@mail.ru>
2020-10-21 21:25:33 +0700

    webpack settings

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './App.css';
+
+function App() {
+  return (
+    <div className={s.app}>
+      <header className={s.appHeader}>
+        <p>
+          Hello
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
